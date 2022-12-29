b4a62f822c52ff85569e62aafaadfb7126ff5af1
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-12-29 11:22:24 -0600

    Feat: Add examples and notes about Introduction to LINQ

--- README.md
+++ README.md
@@ -1,12 +1,87 @@
 # LINQ
+
 Repository to practice LINQ
 
 ## What is LINQ (Language Integrated Query)?
+
 Is a set of technologies that allow simple and efficient querying over differtent kinds of data.
 
 LINQ is able to work with other things than Arrays or List, like Databases, XML files,
 
 ## Benefits of LINQ
-* It allows a simple, efficient and unified way of queryying over different types of data
-* Queries are execute only when the result is needed
-* Code written with LINQ is cleaner, more readable and shorter
+
+- It allows a simple, efficient and unified way of queryying over different types of data
+- Queries are execute only when the result is needed
+- Code written with LINQ is cleaner, more readable and shorter
+
+## What is Lambda expressions?
+
+These expressions are part of C# and are used to create anonymous functions.
+The sintaxis of a lambda expression is the following:
+
+OneLineFunction
+
+**(input params)** + **(lambda declaration operator)** => + **(result of the function)**
+
+MoreLinesFunction
+
+**(input params)** + **(lambda declaration operator)** => + { return **(result of the function)**}
+
+## LINQ and ExtensionMethods
+
+### What is an ExtensionMethod?
+
+Is a method defined outside of a type, that can be called upon this type's objects as a regular member
+method. Extension methods allow us to add new functionality to a type without modifying it.
+
+## LINQ and IEnumerable<T>
+
+IEnumerable<T> is an interface implemented by most of the collections (Arrays, Lists, HashSets and Dictionaries).
+This interface enables iterating a collection with a foreach loop. If a type doesnnt implement this interface
+it's not possible to iterate it with a foreach loop.
+
+_Most of the LINQ's methods are extension methods for IEnumerable<T>_
+
+IEnumerable doesn't expose any methods for collection modification. **This means LINQ methods will never modify
+the input collections**.
+The **Append** method actually doesn't modify the collection, it creates another one with the new element instead
+
+There are more LINQ methods that creates a new collection like: Where, OrderBy.
+
+### Chaining methods
+
+There are some methods that produce a new IEnumerable<T> as result, we can chaining LINQ methods using this
+feature. I mean we can execute a Where operation and with that result execute a OrderBy operation inmediatly
+and in the same line of code.
+
+## Deferred Execution
+
+This kind of execution means:
+* That the evaluation of a LINQ expression is delayed until the value is actually needed.
+* It improves the performance by avoiding unnecessary execution/materialization.
+* Allow us to work with the latest data
+
+
+Knowing about how LINQ truly works when it comes to bigger chains is an amazing piece of knowledge which immensely
+helps when it comes to performance estimations & optimizations.
+
+If I had to mention two things to take away from this post:
+
+- Imagine LINQ to work like a continuous stream instead of a monstrosity which constantly enumerates your source sequence.
+- Be thoughtful when and where to materialize your LINQ queries. Lots of .ToList() calls cause a lot of potentially unnecessary materialization.
+
+## Method Syntax vs Query Syntax
+
+### Method Syntax
+#### Advantages
+* It's pure C#
+#### Disadvantages
+
+
+### Query Syntax
+#### Advantages
+* Easy to learn if you know SQL
+* Some operations are simple to write when we using it (like Join operation)
+#### Disadvantages
+* Has a new "language" to learn
+* Not all LINQ operations are supported (like Distinct operation) and at the end we will need apply method syntax
--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,9 +1,28 @@
 ﻿using Exercises;
 namespace ConsoleSample
 {
+    public static class Extensions
+    {
+        public static int GetCountOfLines(this string input)
+        {
+            return input.Split("\n").Length;
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public Person(string _name, string _country)
+        {
+            Name = _name;
+            Country = _country;
+        }
+    }
+
     internal class Program
     {
-
         static void Main(string[] args)
         {
             //METHOD SYNTAX
@@ -47,21 +66,143 @@ namespace ConsoleSample
             //OTHERS
             //DotNet6Improvements.Run();
 
-            var wordsNoUpperCase = new string[] {
-                "quick", "brown", "fox"
-            };
-            //Console.WriteLine(IsAnyWordUpperCase(wordsNoUpperCase));    
-            Console.WriteLine(IsAnyWordUpperCase_LINQ(wordsNoUpperCase));    
-            var wordsWithUpperCase = new string[] {
-                "quick", "brown", "FOX"
-            };
-            //Console.WriteLine(IsAnyWordUpperCase(wordsWithUpperCase));
-            Console.WriteLine(IsAnyWordUpperCase_LINQ(wordsWithUpperCase));
+            #region Introduction to LINQ
+            //var wordsNoUpperCase = new string[] {
+            //    "quick", "brown", "fox"
+            //};
+            ////Console.WriteLine(IsAnyWordUpperCase(wordsNoUpperCase));    
+            //Console.WriteLine(IsAnyWordUpperCase_LINQ(wordsNoUpperCase));
+            //var wordsWithUpperCase = new string[] {
+            //    "quick", "brown", "FOX"
+            //};
+            ////Console.WriteLine(IsAnyWordUpperCase(wordsWithUpperCase));
+            //Console.WriteLine(IsAnyWordUpperCase_LINQ(wordsWithUpperCase));
 
-            var orderedWords = wordsNoUpperCase.OrderBy(word => word);
+            //var orderedWords = wordsNoUpperCase.OrderBy(word => word);
+            #endregion
+
+            #region Introduction to Lambda Expressions
+            var numbers = new[] { 1, 4, 3, 99, 256, 2 };
+            //bool isAnyLargerThan100 = IsAny(numbers, number => {
+            //    const int max = 100;
+            //    return number > max;
+            //});
+            //Console.WriteLine($"Is any > 100?: {isAnyLargerThan100}");
+
+            //Func<int, bool> isEven = number => number % 2 == 0;
+            //bool isAnyEven = IsAny(numbers, isEven );
+            //Console.WriteLine($"Is any even?: {isAnyEven}");
 
 
+            //var words = new[] { "aaa", "bbbb", "cc" };
+            //bool isAnyOfLength4 = IsAny(words, word => word.Length == 4);
+            //Console.WriteLine($"Is any word length == 4?: {isAnyOfLength4}");
 
+            #endregion
+
+            #region LINQ and ExtensionMethods
+            //var words = new List<string>() { "a", "bb", "ccc", "dddd" };
+            //var wordsLongerThan2Letters = words.Where(word => word.Length > 2);
+            //Console.WriteLine(string.Join(", ", wordsLongerThan2Letters));
+
+            //var multiLineString = @"
+            //Please give me a ride on your back,
+            //Said the duck to the kangaroo:
+            //I would sit quite still, and say nothing but 'quack'
+            //The whole of the long day through;
+            //And we'd go the Dee, and the Jelly Bo Lee,
+            //Over the land, and over the sea:
+            //Please take me a ridde! oh, do!
+            //Said the duck to the kangaroo.";
+
+            //var countOfLines = multiLineString.GetCountOfLines();
+            //Console.WriteLine($"This paragraph {multiLineString} \n has {countOfLines} lines");
+            #endregion
+
+            #region LINQ and IEnumerable<T>
+            //var numbers = new List<int> { 5, 3, 7, 1, 2, 4 };
+            //var numnerswith10 = numbers.Append(10);
+
+            //Console.WriteLine("Numbers: " + string.Join(", ", numbers));
+            //Console.WriteLine("Numbers with 10 : " + string.Join(", ", numnerswith10));
+
+            ////var oddNumbers = numbers.Where(number => number % 2 == 1);
+            //var orderedOddNumers = numbers
+            //    .Where(number => number % 2 == 1)
+            //    .OrderBy(number => number);
+            //Console.WriteLine("Odd Numbers: " + string.Join(", ", orderedOddNumers));
+            #endregion
+            #region DeferredExecution
+            //var shortWords = words.Where(word => word.Length < 3);
+            //Console.WriteLine("First iteration");
+            //foreach (var word in shortWords)
+            //{
+            //    Console.WriteLine(word);
+            //}
+
+            //words.Add("e");
+
+            //Console.WriteLine("Second iteration");
+            //foreach (var word in shortWords)
+            //{
+            //    Console.WriteLine(word);
+            //}
+
+            //var people = new List<Person>
+            //{
+            //    new Person("Jhon", "USA"),
+            //    new Person("Betty", "UK")
+            //};
+
+            //var allAmericans = people.Where(person => person.Country == "USA");
+            //var notAllAmericans = allAmericans.Take(100);
+
+            //var animals = new List<string>()
+            //{
+            //    "Duck","Lion","Dolphin","Tiger"
+            //};
+
+            //var animalsWithD = animals.Where(animal =>
+            //{
+            //    Console.WriteLine("Checking animal:" + animal);
+            //    return animal.StartsWith("D");
+            //});
+
+            //foreach(var animal in animalsWithD)
+            //{
+            //    Console.WriteLine(animal);
+            //}
+            #endregion
+
+            #region Method vs Query Syntax
+            numbers = new[] { 4, 2, 7, 10, 12, 5, 4, 2 };
+
+            var smallOrderedNumbersMethodSyntax = numbers
+                .Where(number => number < 10)
+                .OrderBy(number => number)
+                .Distinct();
+            Console.WriteLine(String.Join(", ", smallOrderedNumbersMethodSyntax));
+
+            var smallOrderedNumbersQuerySyntax = (from number in numbers
+                                                 where number < 10
+                                                 orderby number
+                                                 select number).Distinct();
+            Console.WriteLine(String.Join(", ", smallOrderedNumbersQuerySyntax));
+
+            #endregion
+
+        }
+
+
+        public static bool IsAny<T>(
+            IEnumerable<T> collection,
+            Func<T, bool> predicate)
+        {
+            foreach (var intem in collection)
+            {
+                if (predicate(intem)) return true;
+            }
+            return false;
         }
 
         public static bool IsAnyWordUpperCase_LINQ(IEnumerable<string> words)
@@ -70,17 +211,16 @@ namespace ConsoleSample
                 word.All(letter => char.IsUpper(letter)));
         }
 
-
         public static bool IsAnyWordUpperCase(IEnumerable<string> words)
         {
             foreach (string word in words)
             {
                 bool areAllUpperCase = true;
-                foreach(var letter in word)
+                foreach (var letter in word)
                 {
-                    if(char.IsLower(letter)) areAllUpperCase = false;
+                    if (char.IsLower(letter)) areAllUpperCase = false;
                 }
-                if(areAllUpperCase) return true;
+                if (areAllUpperCase) return true;
             }
             return false;
         }
